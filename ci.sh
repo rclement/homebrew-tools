e28af5c9030062307e9e51911d40d751d8258422
Romain Clement <r.clement88@gmail.com>
2018-09-05 00:05:24 +0200

    Install homebrew formulae in CI script

--- ci.sh
+++ ci.sh
@@ -1,18 +1,23 @@
 #!/usr/bin/env bash
-set -ex
+set -eo pipefail; [[ $TRACE ]] && set -x
 
 function add_homebrew_tap {
-    echo "Adding homebrew tap: $1"
+    echo "--> Adding homebrew tap: $1"
     brew tap $1
 }
 
+function install_homebrew_formula {
+    echo "--> Installing homebrew formula: $1"
+    brew install $1
+}
+
 function test_homebrew_formula {
-    echo "Testing homebrew formula: $1"
+    echo "--> Testing homebrew formula: $1"
     brew test $1
 }
 
 function audit_homebrew_formula {
-    echo "Auditing homebrew formula: $1"
+    echo "--> Auditing homebrew formula: $1"
     brew audit --strict $1
 }
 
@@ -24,6 +29,7 @@ function main {
 
     for f in $formulae
     do
+        install_homebrew_formula $f
         test_homebrew_formula $f
         audit_homebrew_formula $f
     done

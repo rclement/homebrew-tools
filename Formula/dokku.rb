19134fd885e8e07e00298c4b4354e43109399ec9
Romain Clement <r.clement88@gmail.com>
2018-10-02 11:39:58 +0200

    dokku 0.12.13

--- Formula/dokku.rb
+++ Formula/dokku.rb
@@ -1,8 +1,8 @@
 class Dokku < Formula
   desc "Command-line client for the Dokku PaaS"
   homepage "http://dokku.viewdocs.io"
-  url "https://github.com/dokku/dokku/archive/v0.12.12.tar.gz"
-  sha256 "40137e1abdf7ccf78be5cf00d9800b10992f660c1a56bbed086ae2085037ad78"
+  url "https://github.com/dokku/dokku/archive/v0.12.13.tar.gz"
+  sha256 "4759f953a3cf6810323b354e1e593f52e40114230091fecd0f5ed6c6a352d539"
 
   def install
     bin.install "contrib/dokku_client.sh" => "dokku"
